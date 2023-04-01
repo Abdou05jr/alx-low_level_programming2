0142d786fd972b3abd5e860edf88ffa617865763
Abdou <aboudiwac9@gmail.com>
2023-03-31 18:47:58 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,22 @@
-#include "putchar.c"
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	char *sh = "Putchar";
+#include "main.h"
 
-	while (*sh)
-	{
-		_putchar(*sh);
-		sh++;
-	}
+/**
+ * main - Entry point 
+ * Return: Always 0
+ */
+
+int main(void)
+
+{
+
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
